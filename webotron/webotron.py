22af57aabe173b76f5f8a4fdc33d511b2985d566
liamwoodroberts <lwoodroberts@diginsights.com>
2020-04-16 10:14:48 -0400

    added s3 sync feature

--- webotron/webotron.py
+++ webotron/webotron.py
@@ -1,8 +1,12 @@
 import boto3
 import sys
 import click
+import mimetypes
 
-session = boto3.Session(profile_name="Liam")
+from botocore.exceptions import ClientError
+from pathlib import Path
+
+session = boto3.Session(profile_name="Liam",region_name="us-east-1")
 s3 = session.resource("s3")
 
 @click.group()
@@ -24,5 +28,69 @@ def list_bucket_objects(bucket):
         print(obj)
     pass
 
+@cli.command("setup-bucket")
+@click.argument("bucket")
+def setup_bucket(bucket):
+    "Create and configure S3 bucket"
+    s3_bucket = None
+
+    try:
+        s3_bucket = s3.create_bucket(Bucket=bucket)
+
+    except ClientError as e:
+        if e.response["Error"]["Code"] == "BucketAlreadyOwnedByYou":
+            s3_bucket = s3.Bucket(bucket)
+        else:
+            raise e
+    policy = '''{
+                  "Version":"2012-10-17",
+                    "Statement":[{
+                        "Sid":"PublicReadGetObject",
+                        "Effect":"Allow",
+                        "Principal": "*",
+                        "Action":["s3:GetObject"],
+                        "Resource":["arn:aws:s3:::%s/*"]
+                        }
+                    ]
+                }''' % s3_bucket.name
+
+    policy = policy.strip()
+    pol = s3_bucket.Policy()
+    pol.put(Policy=policy)
+    ws = s3_bucket.Website()
+    ws.put(WebsiteConfiguration={
+        'ErrorDocument': {
+            'Key': 'error.html' },
+        'IndexDocument': {
+            'Suffix': 'index.html'}})
+    return
+
+def upload_file(s3_bucket,path,key):
+    content_type = mimetypes.guess_type(key)[0] or "text/plain"
+    s3_bucket.upload_file(
+        path,
+        key,
+        ExtraArgs={
+            "ContentType": content_type
+        })
+    return
+@cli.command('sync')
+@click.argument('pathname',type=click.Path(exists=True))
+@click.argument('bucket')
+def sync(pathname,bucket):
+    "Sync contents of PATHNAME to BUCKET"
+    s3_bucket = s3.Bucket(bucket)
+
+    root = Path(pathname).expanduser().resolve()
+
+    def handle_directory(target):
+        for p in target.iterdir():
+            if p.is_dir():
+                handle_directory(p)
+            elif p.is_file():
+                upload_file(s3_bucket,str(p),str(p.relative_to(root)))
+
+    handle_directory(root)
+    #url = "https://%s.s3-website.us-east-1.amazonaws.com" % new_bucket.name
 if __name__ == "__main__":
     cli()
